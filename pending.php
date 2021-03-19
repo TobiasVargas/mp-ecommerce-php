57ecda1a0fdad3aab93525b402a271052d9696fb
Tobias de Vargas <42094615+TobiasVargas@users.noreply.github.com>
2021-03-18 23:58:19 -0300

    integração realizada

---
+++ pending.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="viewport" content="width=1024">
+    <title>Tienda e-commerce</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="format-detection" content="telephone=no">
+</head>
+<body>
+    
+
+    <script src="https://www.mercadopago.com/v2/security.js" view=""></script>
+
+</body>
+</html>
